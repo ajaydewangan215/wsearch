2c1b96193fe725034192a7f6a4f656752abedb7e
Ajay Dewangan <ajaydewangan215@gmail.com>
2023-05-23 13:06:36 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages: any = []
+  constructor(private wiki: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wiki.search(term).subscribe(pages => {
+      this.pages = pages;
+    })
+  }
 }
